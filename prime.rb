24ef3ed02fe89eb5b5f425ed4c74036e6777ff5d
Jonathan <ruizjayy@icloud.com>
2021-03-07 23:15:02 -0600

    commit tree e3e06043447b7009cc4a00891638bf6a620cc726

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+require "pry"
+def prime?(num)
+    n = 2
+    while n < num
+      return false if num % n == 0
+      n += 1
+    end
+    if num <= 1
+        false
+    else
+        true
+    end
+end
